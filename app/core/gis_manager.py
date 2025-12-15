dfc9f09a370a8b02aa9826312c1711fa5d052b82
Abdulrahman Ghoneim <aa.elshahat@wakeb.tech>
2025-12-15 16:05:48 +0200

    fix: resolve feature_id issue

--- app/core/gis_manager.py
+++ app/core/gis_manager.py
@@ -8,17 +8,26 @@ from app.config import DATA_DIR
 
 class GISManager:
     def __init__(self, crs="EPSG:4326"):\
-        self.gdf = gpd.GeoDataFrame(column=["properties", "geometry"], crs=crs)
+        self.gdf = gpd.GeoDataFrame(columns=["feature_id", "properties", "geometry"], crs=crs)
 
 
     # add feature
     def add_feature(self, geom_dict, properties: dict):
         geom = shape(geom_dict)
+
         if not geom.is_valid:
             geom = make_valid(geom)
 
-        new_raw = {"properties": properties, "geometry": geom}
-        self.gdf.loc[len(self.gdf)] = new_raw
+        feature_id = len(self.gdf) + 1 
+        new_row = {
+            "feature_id": feature_id,
+            "properties": properties,
+            "geometry": geom
+        }
+
+        self.gdf.loc[len(self.gdf)] = new_row
+        return feature_id
+
 
 
 
@@ -32,6 +41,12 @@ class GISManager:
     #         return False
 
 
+    def delete_feature(self, index: int):
+        if index in self.gdf.index:
+            self.gdf = self.gdf.drop(index).reset_index(drop=True)
+            return True
+        return False
+
     # reproject to target_crs = EPSG:4326
     def reproject(self, target_crs ="EPSG:4326"):
         if self.gdf.crs != target_crs:
@@ -44,12 +59,20 @@ class GISManager:
         print(self.gdf)
 
 
-
+    # save file 
     def save(self, filename ="output.geojson"):
         path = os.path.join(DATA_DIR, filename)
         self.gdf.to_file(path, driver="Geojson")
         return path
 
+    # load_dataset
+    def load_dataset(self, file_path: str):
+        self.gdf = gpd.read_file(file_path)
+
+        if self.gdf.crs is None:
+            self.gdf.set_crs("EPSG:4326", inplace=True)
+
+        return self.gdf
 
 
     # buffer
@@ -78,9 +101,10 @@ class GISManager:
 
     # simplification =>> tolerance, simplify_coverage, simplify_boundary ==> TRUE
     # inner edges only =>>> simplify_boundary => fales 
+
     def simplification(self,tolerance: float, simplify_coverage: bool = True, simplify_boundary: bool = True ):
         if simplify_coverage:
-            self.gdf["geometry"] = self.gdf.geometry.simplify_coverage(tolerance=tolerance, simplify_boundary=simplify_coverage)
+            self.gdf["geometry"] = self.gdf.geometry.simplify_coverage(tolerance=tolerance, simplify_boundary=simplify_boundary)
         else:
             self.gdf["geometry"] = self.gdf.geometry.simplify(tolerance)
         return self.gdf
@@ -88,7 +112,7 @@ class GISManager:
 
 
     # dissolve by attribute 
-    def dissolve(self, by: str)
+    def dissolve(self, by: str):
         self.gdf = self.gdf.dissolve(by=by, as_index=False)
         return self.gdf 
     
